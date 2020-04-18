3100ff074db4b9ddfe130a823fefe975c2f011a8
dreamer1995 <dreamer199506@126.com>
2020-04-18 23:21:31 +0800

    Submit puzzle game

--- Alive/Assets/Scripts/GameController.cs
+++ Alive/Assets/Scripts/GameController.cs
@@ -1,27 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public GameObject enemy;
     public float enemyInterval;
     public GameObject spawnArea1;
+    public float branch;
+    public GameObject grid;
+    public int[,] puzzleMap;
+    public int[,] answerMap;
+    public bool puzzleClear;
+    public Text text;
+    public Text text2;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnEnemy());
+        puzzleClear = false;
+        puzzleMap = new int[5, 5] {
+            { 0,0,0,0,0 },
+            { 0,0,0,0,0 },
+            { 0,0,0,0,0 },
+            { 0,0,0,0,0 },
+            { 0,0,0,0,0 }
+        };
+        answerMap = new int[5,5] {
+            { 1,0,0,0,1 },
+            { 0,0,0,0,0 },
+            { 0,1,0,1,0 },
+            { 1,0,0,0,1 },
+            { 1,1,0,1,1 }
+        };
+        if (branch == 0) 
+        {
+            StartCoroutine(SpawnEnemy());
+        }
+        else
+        {
+            StartCoroutine(GenerateGrids(puzzleMap));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(puzzleClear)
+        {
+            text2.gameObject.SetActive(true);
+        }
     }
 
     IEnumerator SpawnEnemy()
     {
-        while(true)
+        while (true)
         {
             Vector3 minPoint = spawnArea1.transform.position - spawnArea1.transform.localScale / 2;
             Vector3 maxPoint = spawnArea1.transform.position + spawnArea1.transform.localScale / 2;
@@ -31,5 +64,20 @@ public class GameController : MonoBehaviour
             Instantiate(enemy, spawnPos, Quaternion.identity);
             yield return new WaitForSeconds(enemyInterval);
         }
+    }
+    IEnumerator GenerateGrids(int[,] puzzleMap)
+    {
+        text.gameObject.SetActive(false);
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                GameObject _grid = Instantiate(grid, new Vector3(-27.0f + j, 0.5f, 2.0f - i), Quaternion.identity);
+                _grid.name = i + "" + j;
+                //if(puzzleMap[i,j] == 1)
+                //    _grid.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+            }
+        }
+        yield return new WaitForSeconds(0);
     }
 }
---
+++ Alive/Assets/Scripts/GridAI.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GridAI : MonoBehaviour
+{
+    private GameObject cam;
+    private GameController gameController;
+    private Material mt;
+    private int i;
+    private int j;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera");
+        int num;
+        int.TryParse(name, out num);
+        i = num / 10;
+        j = num % 10;
+        mt = GetComponent<Renderer>().material;
+        gameController = cam.GetComponent<GameController>();
+        if (gameController.puzzleMap[i,j] == 1)
+        {
+            mt.SetColor("_Color", Color.blue);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        if(gameController.puzzleMap[i, j] == 0)
+        {
+            GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+            gameController.puzzleMap[i, j] = 1;
+        }
+        else
+        {
+            GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+            gameController.puzzleMap[i, j] = 0;
+        }
+        if(gameController.puzzleMap[i, j] == gameController.answerMap[i, j])
+        {
+            bool equal = true;
+            for (int k = 0; k < 5; k++)
+            {
+                for (int l = 0; l < 5; l++)
+                {
+                    if (gameController.puzzleMap[k, l] != gameController.answerMap[k, l])
+                    {
+                        equal = false;
+                        break;
+                    }
+                }
+                if(!equal)
+                {
+                    break;
+                }
+            }
+            if(equal)
+            {
+                gameController.puzzleClear = true;
+            }
+        }
+
+    }
+}
